f41e5e2c28a2dfa83b9354ae7a9542a4adf8b5c2
Tolga Arslan <eng.tolgaarslan@gmail.com>
2023-05-11 14:15:31 +0300

    position control

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import turtle
 import time
 from random import randint
 
+score = 0
+
+def position_control(x,y):
+    # print(f"""
+    # x kordinatı = {x_cor}
+    # y kordinatı = {y_cor}
+    # mouse x kordinatı = {x}
+    # mouse y kordinatı = {y}
+    # """)
+    #
+    # if (x_cor-20) <= int(x) <= (x_cor+20) and (y_cor-20) <= int(y) <= (y_cor+20):
+    #     global score
+    #     score = score + 1
+    # print(score)
+    pass
 
 turtle_screen = turtle.Screen()
 turtle_screen.bgcolor("light blue")
@@ -15,16 +30,14 @@ turtle_instance.shapesize(3)
 turtle_instance.penup()
 
 
-while True:
-    x_cor = randint(-400, 400)
-    y_cor = randint(-300, 300)
-    turtle_instance.hideturtle()
-    turtle_instance.setposition(x_cor, y_cor)
-    turtle_instance.showturtle()
-    time.sleep(0.3)
-
-
-
+for i in range(0,200):
+    # x_cor = randint(-400, 400)
+    # y_cor = randint(-300, 300)
+    # turtle_instance.hideturtle()
+    turtle_instance.setposition(i, i)
+    # turtle_instance.showturtle()
+    turtle_screen.onclick(position_control)
+    time.sleep(10)
 
 turtle_screen.mainloop()
 
