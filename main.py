88806ed5bb144ba6bcae89705c6c282e940c2038
Tolga Arslan <eng.tolgaarslan@gmail.com>
2023-05-11 11:29:46 +0300

    Turtle Picture added

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import turtle
+import time
 
 turtle_screen = turtle.Screen()
 turtle_screen.bgcolor("light blue")
 turtle_screen.title("Catch The Turtle Game")
 
 turtle_instance = turtle.Turtle()
+
+turtle_instance.hideturtle()
+time.sleep(2)
+turtle_instance.showturtle()
+
 
 turtle.done()
 
