cbefd88d8dc9299aa4b8bcd3d408122e716ae7c8
Tolga Arslan <eng.tolgaarslan@gmail.com>
2023-05-11 11:22:21 +0300

    Turtle screen is created

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import turtle
 
+turtle_screen = turtle.Screen()
+turtle_screen.bgcolor("light blue")
+turtle_screen.title("Catch The Turtle Game")
+
+turtle_instance = turtle.Turtle()
+
+turtle.done()
 
 
 
