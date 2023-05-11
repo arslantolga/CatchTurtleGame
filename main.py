ebe400b1054d9d34703457ecacda8aac2f78b739
Tolga Arslan <eng.tolgaarslan@gmail.com>
2023-05-11 12:12:46 +0300

    Turtle is moved random position

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle
 import time
+from random import randint
+
 
 turtle_screen = turtle.Screen()
 turtle_screen.bgcolor("light blue")
@@ -7,12 +9,24 @@ turtle_screen.title("Catch The Turtle Game")
 
 turtle_instance = turtle.Turtle()
 
-turtle_instance.hideturtle()
-time.sleep(2)
-turtle_instance.showturtle()
+turtle_instance.shape("turtle")
+turtle_instance.color("green")
+turtle_instance.shapesize(3)
+turtle_instance.penup()
 
 
-turtle.done()
+while True:
+    x_cor = randint(-400, 400)
+    y_cor = randint(-300, 300)
+    turtle_instance.hideturtle()
+    turtle_instance.setposition(x_cor, y_cor)
+    turtle_instance.showturtle()
+    time.sleep(0.3)
+
+
+
+
+turtle_screen.mainloop()
 
 
 
